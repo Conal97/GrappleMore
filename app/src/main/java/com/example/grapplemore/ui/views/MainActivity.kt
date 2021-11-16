f0199cd9568413204198d020506ba905a59e00a0
Conal97 <conalbrosnan@gmail.com>
2021-11-16 21:12:35 +0000

    Nav drawer implementation.

--- app/src/main/java/com/example/grapplemore/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/grapplemore/ui/views/MainActivity.kt
@@ -4,12 +4,24 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.grapplemore.R
+import com.example.grapplemore.databinding.ActivityMainBinding
+import com.example.grapplemore.databinding.LoginFragmentBinding
 import com.google.android.play.core.internal.t
 import com.google.android.play.core.splitinstall.d
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,32 +32,22 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
+   lateinit var toggle: ActionBarDrawerToggle
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Navigation with the navigation drawer
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.findNavController()
 
         toggle = ActionBarDrawerToggle(this, drawer_layout, R.string.open, R.string.close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.navUserProfile -> Toast.makeText(applicationContext, "Clicked User profile", Toast.LENGTH_SHORT).show()
-                R.id.navNote -> Toast.makeText(applicationContext, "Clicked User profile", Toast.LENGTH_SHORT).show()
-            }
-            true
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true
-        }
-        return super.onOptionsItemSelected(item)
+        navView.setupWithNavController(navController)
     }
 }
