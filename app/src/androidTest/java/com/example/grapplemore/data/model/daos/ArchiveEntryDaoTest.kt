a691ebcdb1d300cb3809c258f6fabd4cbb653108
Conal97 <conalbrosnan@gmail.com>
2021-12-09 21:12:51 +0000

    Unit tests for archive entry dao and rolling footage dao

---
+++ app/src/androidTest/java/com/example/grapplemore/data/model/daos/ArchiveEntryDaoTest.kt
@@ -0,0 +1,130 @@
+package com.example.grapplemore.data.model.daos
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.filters.SmallTest
+import com.example.grapplemore.data.model.AppDatabase
+import com.example.grapplemore.data.model.entities.ArchiveEntry
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
+
+
+@ExperimentalCoroutinesApi
+@SmallTest
+@HiltAndroidTest
+class ArchiveEntryDaoTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    @Named("test_db")
+    lateinit var database: AppDatabase
+    private lateinit var archiveEntryDao: ArchiveEntryDao
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        archiveEntryDao = database.getArchiveEntryDao()
+    }
+
+    @After
+    fun teardown(){
+        database.close()
+    }
+
+    /*
+    Tests to confirm the functionality of the database and archive entry dao
+     */
+
+    @Test
+    fun insertArchiveEntry() = runBlockingTest {
+
+        val archiveEntry = ArchiveEntry(1, "TestOne", "TestOne", "TestOne",
+            "TestOne", "TestOne")
+        archiveEntryDao.upsertArchiveEntry(archiveEntry)
+        val allArchiveEntries = archiveEntryDao.getAllUserArchiveEntries("TestOne").getOrAwaitValue()
+
+        assertThat(allArchiveEntries).contains(archiveEntry)
+    }
+
+    @Test
+    fun getEntriesByID() = runBlocking {
+
+        val archiveEntry = ArchiveEntry(1, "TestOne", "TestOne", "TestOne",
+            "TestOne", "TestOne")
+        archiveEntryDao.upsertArchiveEntry(archiveEntry)
+        val allArchiveEntries = archiveEntryDao.getAllUserArchiveEntries("TestTwo").getOrAwaitValue()
+
+        assertThat(allArchiveEntries).doesNotContain(archiveEntry)
+    }
+
+    @Test
+    fun deleteEntry() = runBlocking {
+
+        val archiveEntry = ArchiveEntry(1, "TestOne", "TestOne", "TestOne",
+            "TestOne", "TestOne")
+        archiveEntryDao.upsertArchiveEntry(archiveEntry)
+        archiveEntryDao.deleteArchiveEntry(archiveEntry)
+        val allArchiveEntries = archiveEntryDao.getAllUserArchiveEntries("TestOne").getOrAwaitValue()
+
+        assertThat(allArchiveEntries).doesNotContain(archiveEntry)
+    }
+
+    @Test
+    fun getByTitle() = runBlocking {
+
+        val archiveEntry = ArchiveEntry(1, "TestOne", "TestOne", "TestOne",
+            "TestOne", "TestOne")
+        archiveEntryDao.upsertArchiveEntry(archiveEntry)
+
+        val correctEntries = archiveEntryDao.getByTitle("TestOne", "Test").getOrAwaitValue()
+        val incorrectEntries = archiveEntryDao.getByTitle("TestOne", "TestTwo").getOrAwaitValue()
+
+        assertThat(correctEntries).contains(archiveEntry)
+        assertThat(incorrectEntries).doesNotContain(archiveEntry)
+    }
+
+    @Test
+    fun getByCategory() = runBlocking{
+
+        val archiveEntry = ArchiveEntry(1, "TestOne", "TestOne", "TestOne",
+            "TestOne", "TestOne")
+        archiveEntryDao.upsertArchiveEntry(archiveEntry)
+
+        val correctEntries = archiveEntryDao.getByCategory("TestOne", "TestOne").getOrAwaitValue()
+        val incorrectEntries = archiveEntryDao.getByCategory("TestOne", "TestTwo").getOrAwaitValue()
+
+        assertThat(correctEntries).contains(archiveEntry)
+        assertThat(incorrectEntries).doesNotContain(archiveEntry)
+    }
+
+    @Test
+    fun getByCategoryAndTitle() = runBlocking {
+
+        val archiveEntry = ArchiveEntry(1, "TestOne", "TestOne", "TestOne",
+            "TestOne", "TestOne")
+        archiveEntryDao.upsertArchiveEntry(archiveEntry)
+
+        val correctEntries = archiveEntryDao.getByCategoryAndTitle("TestOne", "TestOne", "TestOne").getOrAwaitValue()
+        val incorrectTitle = archiveEntryDao.getByCategoryAndTitle("TestOne", "TestTwo", "TestOne").getOrAwaitValue()
+        val incorrectCategory = archiveEntryDao.getByCategoryAndTitle("TestOne", "TestOne", "TestTwo").getOrAwaitValue()
+
+        assertThat(correctEntries).contains(archiveEntry)
+        assertThat(incorrectTitle).doesNotContain(archiveEntry)
+        assertThat(incorrectCategory).doesNotContain(archiveEntry)
+    }
+}
---
+++ app/src/androidTest/java/com/example/grapplemore/data/model/daos/RollingFootageDaoTest.kt
@@ -0,0 +1,121 @@
+package com.example.grapplemore.data.model.daos
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.filters.SmallTest
+import com.example.grapplemore.data.model.AppDatabase
+import com.example.grapplemore.data.model.entities.RollingFootage
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
+
+
+@ExperimentalCoroutinesApi
+@SmallTest
+@HiltAndroidTest
+class RollingFootageDaoTest {
+
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    @Named("test_db")
+    lateinit var database: AppDatabase
+    private lateinit var rollingFootageDao: RollingFootageDao
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        rollingFootageDao = database.getRollingFootageDao()
+    }
+
+    @After
+    fun teardown(){
+        database.close()
+    }
+
+    /*
+    Tests to confirm the functionality of the database and rolling footage dao
+     */
+
+    @Test
+    fun insertRollingFootage() = runBlocking {
+
+        val testFootage = RollingFootage(1, "testOne","testOne",
+            "testOne","testOne")
+
+        rollingFootageDao.insertRollingFootage(testFootage)
+        val allRollingFootage = rollingFootageDao.getAll("testOne").getOrAwaitValue()
+
+        assertThat(allRollingFootage).contains(testFootage)
+    }
+
+    @Test
+    fun deleteRollingFootage() = runBlocking {
+
+        val testFootage = RollingFootage(1, "testOne","testOne",
+            "testOne","testOne")
+
+        rollingFootageDao.insertRollingFootage(testFootage)
+        rollingFootageDao.deleteRollingFootage(testFootage)
+        val allRollingFootage = rollingFootageDao.getAll("testOne").getOrAwaitValue()
+
+        assertThat(allRollingFootage).doesNotContain(testFootage)
+    }
+
+    @Test
+    fun footagePerUser() = runBlocking {
+
+        val testFootageOne = RollingFootage(1, "testOne","testOne",
+            "testOne","testOne")
+
+        val testFootageTwo = RollingFootage(2, "testTwo", "testTwo",
+        "testTwo", "testTwo")
+
+        rollingFootageDao.insertRollingFootage(testFootageOne)
+        rollingFootageDao.insertRollingFootage(testFootageTwo)
+
+        val userOneFootage = rollingFootageDao.getAll("testOne").getOrAwaitValue()
+        val userTwoFootage = rollingFootageDao.getAll("testTwo").getOrAwaitValue()
+
+        assertThat(userOneFootage).contains(testFootageOne)
+        assertThat(userTwoFootage).contains(testFootageTwo)
+
+        assertThat(userOneFootage).doesNotContain(testFootageTwo)
+        assertThat(userTwoFootage).doesNotContain(testFootageOne)
+
+    }
+
+    @Test
+    fun footageByTitle() = runBlocking {
+
+        val testFootageOne = RollingFootage(1, "testOne","testOne",
+            "testOne","testOne")
+
+        val testFootageTwo = RollingFootage(2, "testTwo", "testTwo",
+            "testTwo", "testOne")
+
+        rollingFootageDao.insertRollingFootage(testFootageOne)
+        rollingFootageDao.insertRollingFootage(testFootageTwo)
+
+        val titleFirst = rollingFootageDao.getFootageByTitle("testOne", "test").getOrAwaitValue()
+        val titleSecond = rollingFootageDao.getFootageByTitle("testOne", "testTwo").getOrAwaitValue()
+
+        assertThat(titleFirst).contains(testFootageOne)
+        assertThat(titleFirst).contains(testFootageTwo)
+        assertThat(titleSecond).doesNotContain(testFootageOne)
+        assertThat(titleSecond).contains(testFootageTwo)
+    }
+}
